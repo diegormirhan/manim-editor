96574d385129099aa5cafd2a609a0e40dea0feea
diegormirhan <mirhan.diego@gmail.com>
2026-09-07 13:29:15 -0300

    refactor: remove superseded standalone equation compiler

--- renderer/manim_renderer/elements/math_tex.py
+++
@@ -1,8 +0,0 @@
-def compile_math_tex(element: dict, variable: str) -> list[str]:
-    latex = repr(element["latex"])
-    position = repr(element["position"])
-    return [
-        f"{variable} = MathTex({latex})",
-        f"{variable}.move_to({position})",
-    ]
-
